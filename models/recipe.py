53cc806bf0216995286fa86f84bfff939f131f39
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-05-25 16:03:03 -0600

    Updated recipe.py
    Updated model to define a database table called Recipe following Flask-SQLAlchemy

--- models/recipe.py
+++ models/recipe.py
@@ -1,23 +1,40 @@
+from extensions import db
+
 recipe_list = []
 
 def get_last_id():
-    if recipe_list:
-        last_recipe = recipe_list[-1]
-    else:
+    last_recipe = Recipe.query.order_by(Recipe.id.desc()).first()
+
+    if last_recipe is None:
         return 1
 
     return last_recipe.id + 1
 
-class Recipe():
+class Recipe(db.Model):
 
-    def __init__(self, name, description, num_of_servings, cook_time, directions):
-        self.id = get_last_id()
+    __tablename__ = 'recipe'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    num_of_servings = db.Column(db.Integer)
+    cook_time = db.Column(db.Integer)
+    directions = db.Column(db.String(1000))
+    is_publish = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, name, description, num_of_servings, cook_time, directions, user_id):
+        # self.id = get_last_id()
         self.name = name
         self.description = description
         self.num_of_servings = num_of_servings
         self.cook_time = cook_time
         self.directions = directions
         self.is_publish = False
+        self.user_id = user_id
 
     @property
     def data(self):
