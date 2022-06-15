e6e3465ba940cebd6d96bc3cafc6138351cee144
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-06-15 14:38:13 -0600

    Updated app.py
    Added users, me and token resources
    
    Added jwt token blocklist verification

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from config import Config
-from extensions import db
+from extensions import db, jwt
 
-from resources.user import UserListResource
+from resources.user import UserListResource, UserResource, MeResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+from resources.token import TokenResource, RefreshResource, RevokeResource, black_list
 
 def create_app():
     app = Flask(__name__)
@@ -20,11 +21,23 @@ def create_app():
 def register_extensions(app):
     db.init_app(app)
     migrate = Migrate(app, db)
+    jwt.init_app(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(headers, decrypted_data):
+        jti = decrypted_data['jti']
+        return jti in black_list
 
 def register_resources(app):
     api = Api(app)
 
     api.add_resource(UserListResource, '/users')
+    api.add_resource(UserResource, '/users/<string:username>')
+    api.add_resource(MeResource, '/me')
+
+    api.add_resource(TokenResource, '/token')
+    api.add_resource(RefreshResource, '/refresh')
+    api.add_resource(RevokeResource,'/revoke')
 
     api.add_resource(RecipeListResource, '/recipes')
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
