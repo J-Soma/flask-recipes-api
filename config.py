ff3419e28eef1210f253bd8f6e9c47fca160667d
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-06-15 09:36:57 -0600

    Updated config.py
    Fixed os.get_env() to os.getenv()
    Added JWToken configuration for secret keys and blocklist

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ import os
 class Config:
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = os.get_env('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_ERROR_MESSAGE_KEY = 'message'
+
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
